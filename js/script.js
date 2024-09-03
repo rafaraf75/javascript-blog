b74e729936931729dc899de5fe86c22970f550a9
Rafal Labunski <rafallabunski@gmail.com>
2024-09-03 16:46:04 +0200

    Add initial JavaScript functionality for generating article titles, tags, and authors

---
+++ js/script.js
@@ -0,0 +1,96 @@
+'use strict';
+
+// Funkcja do generowania listy tytułów artykułów
+function generateTitleList() {
+    const articles = document.querySelectorAll('.post');
+    const titleList = document.querySelector('.titles');
+    titleList.innerHTML = '';
+
+    articles.forEach(article => {
+        const articleId = article.getAttribute('id');
+        const articleTitle = article.querySelector('.post-title').innerHTML;
+        const linkHTML = `<li><a href="#${articleId}"><span>${articleTitle}</span></a></li>`;
+        titleList.innerHTML += linkHTML;
+    });
+}
+
+// Funkcja do generowania chmury tagów
+function generateTags() {
+    const tags = {};
+    const articles = document.querySelectorAll('.post');
+
+    articles.forEach(article => {
+        const articleTags = article.querySelectorAll('.post-tags a');
+
+        articleTags.forEach(tag => {
+            const tagText = tag.innerHTML;
+            if (!tags[tagText]) {
+                tags[tagText] = 1;
+            } else {
+                tags[tagText]++;
+            }
+        });
+    });
+
+    const tagList = document.querySelector('.tags');
+    tagList.innerHTML = '';
+
+    for (let tag in tags) {
+        const tagHTML = `<li><a href="#">${tag}</a> <span>(${tags[tag]})</span></li>`;
+        tagList.innerHTML += tagHTML;
+    }
+}
+
+// Funkcja do generowania listy autorów
+function generateAuthors() {
+    const authors = {};
+    const articles = document.querySelectorAll('.post');
+
+    articles.forEach(article => {
+        const articleAuthor = article.querySelector('.post-author').innerHTML.replace('by ', '');
+        if (!authors[articleAuthor]) {
+            authors[articleAuthor] = 1;
+        } else {
+            authors[articleAuthor]++;
+        }
+    });
+
+    const authorList = document.querySelector('.authors');
+    authorList.innerHTML = '';
+
+    for (let author in authors) {
+        const authorHTML = `<li><a href="#">${author}</a></li>`;
+        authorList.innerHTML += authorHTML;
+    }
+}
+
+// Funkcja do obsługi kliknięcia na tytuł artykułu
+function handleTitleClick(event) {
+    event.preventDefault();
+    const activeArticles = document.querySelectorAll('.post.active');
+    activeArticles.forEach(article => article.classList.remove('active'));
+
+    const activeLinks = document.querySelectorAll('.titles a.active');
+    activeLinks.forEach(link => link.classList.remove('active'));
+
+    const clickedElement = event.target;
+    clickedElement.classList.add('active');
+
+    const articleSelector = clickedElement.getAttribute('href');
+    const targetArticle = document.querySelector(articleSelector);
+    targetArticle.classList.add('active');
+}
+
+// Dodaj nasłuchiwanie na kliknięcia na liście tytułów
+function addClickListenersToTitles() {
+    const links = document.querySelectorAll('.titles a');
+    links.forEach(link => link.addEventListener('click', handleTitleClick));
+}
+
+// Uruchomienie wszystkich funkcji po załadowaniu DOM
+document.addEventListener('DOMContentLoaded', function() {
+    generateTitleList();
+    generateTags();
+    generateAuthors();
+    addClickListenersToTitles();
+});
